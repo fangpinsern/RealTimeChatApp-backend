9b9a32ae773c7cd58ec50862c2a03b191e7d75d9
fangpinsern <universityforfang@gmail.com>
2021-01-28 04:52:35 +0800

    Push to heroku

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const io = socketio(server, {
   },
 });
 
+app.use(cors());
+
 io.on("connection", (socket) => {
   socket.on("join", ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name: name, room: room });
